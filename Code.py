24cd26364ca29f0543f8022358d442e4aa79c198
Palak Kundu <palakandpranavikundu@gmail.com>
2025-04-14 16:56:00 +0530

    Printing the board

--- Code.py
+++ Code.py
@@ -5,3 +5,12 @@ for i in range(0, 9):
 firstPlayer = True
 winner = False
 iterations = 0 
+
+def printBoard():
+    print('\n=============')
+    print('| ' + choices[0] + ' | ' + choices[1] + ' | ' + choices[2] + ' |')
+    print('=============')
+    print('| ' + choices[3] + ' | ' + choices[4] + ' | ' + choices[5] + ' |')
+    print('=============')
+    print('| ' + choices[6] + ' | ' + choices[7] + ' | ' + choices[8] + ' |')
+    print('=============\n')
